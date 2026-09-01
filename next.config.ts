4980ee7ae2ce6db3f7352ed5553ccc3b5bdca3a9
ali-rashid-dev <alirashid2020e@gmail.com>
2026-09-01 12:23:55 +0500

    feat: add utfs.io and uploadthing.com to next/image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,23 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+        pathname: "/**",
+      },
+      // {
+      //   protocol: "https",
+      //   hostname: "fmcupnguk1.ufs.sh",
+      //   pathname: "/**",
+      // },
     ],
   },
 };
 
 export default nextConfig;
-
